77725459cd0986c2690fb0eb17ad624767f43065
unknown <tarachan@redika.co.in>
2020-11-04 10:35:18 +0530

    New Project

--- upload.bat
+++ upload.bat
@@ -5,6 +5,6 @@ set /p comment="Enter Comment: "
 git commit -m "%comment%"
 git branch -M main
 git remote add origin https://github.com/tarachandDayma/cdinesh.git
-git push -u origin main
+git push -u origin main  --force
 set /p comment1="Enter"
                 
